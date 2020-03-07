82b5e400bf7fd83d834c2d38eb1312e5ad94bbc6
Syed Wafi Hasan <syedwafihasan@gmail.com>
2020-03-08 03:31:40 +0530

    Added colour coded markes. Do exercise 113 next.

--- FoliumProject/map1.py
+++ FoliumProject/map1.py
@@ -1,6 +1,17 @@
 import folium
 import pandas
-map = folium.Map(location=[26.8467, 80.9462], zoom_start=5)
+# TODO : Exercise 113
+def colour_producer(elevation):
+	if elevation < 1000:
+		return 'blue'
+	elif 1000 <= elevation < 2000:
+		return 'green'
+	elif 2000 <= elevation < 3000:
+		return 'orange'
+	else:
+		return 'red'
+
+map = folium.Map(location=[37.0902, -95.7129], zoom_start=5)
 
 data = pandas.read_csv("Volcanoes.txt")
 
@@ -13,8 +24,7 @@ fg = folium.FeatureGroup("My Group")
 
 for lt, ln, n, el in zip(lat, lon, name, elev):
 	s = n + "\n" + str(el) +" metres"
-	fg.add_child(folium.Marker(location=[lt, ln], popup=s, icon=folium.Icon(color="red")))
-
+	fg.add_child(folium.Marker(location=[lt, ln], popup=s, icon=folium.Icon(colour_producer(int(el)))))   
 
 map.add_child(fg)
 
