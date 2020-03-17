88c06af9e3bd6970fb6d659f5fe415f8dad01f07
Syed Wafi Hasan <syedwafihasan@gmail.com>
2020-03-17 18:45:43 +0530

    Deleted resized images

--- OpenCV/face_detection.py
+++ OpenCV/face_detection.py
@@ -1,13 +1,16 @@
 import cv2
+import sys
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 
-img = cv2.imread('p2.jpg')
+i = str(sys.argv[1])
+
+img = cv2.imread(i)
 # cv2.imwrite("ds_small.jpg", cv2.resize(img, (int(img.shape[1]/4), int(img.shape[0]/4))))
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 print(faces)
 
 for x, y, w, h in faces:
